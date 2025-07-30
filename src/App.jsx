4c1cb44f4a11cfc34aeeb48b1553ca0f39509fd3
Tychambers <tylerm.chambers92@gmail.com>
2025-07-30 17:19:11 -0400

    4th commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          <Route path="/TMCStudiosSite" element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
